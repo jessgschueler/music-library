ee4e1263977ea139776ec2d43943a0eeb431b007
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-04-08 09:45:15 -0700

    WIP add validation functions.

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-artist_id_sequence = 100
+from ssl import ALERT_DESCRIPTION_USER_CANCELLED
+
 
 class Artist:
+
+    artist_id_sequence = 100
+
     def __init__(self, artist_id, name, genre, dob):
-        global artist_id_sequence
-        self.artist_id = artist_id_sequence
+        self.artist_id = Artist.artist_id_sequence
         artist_id_sequence += 1
         self.name = name
         self.genre = genre 
         self.dob = dob
+
+    def artist_check(self):
+        for i in artists():
+            if(i == self):
+                print('Artist is already in here!')
+
 
 class Song:
     def __init__(self, artist_id, title, length, lyrics):
@@ -15,4 +24,10 @@ class Song:
         self.title = title
         self.length = length 
         self.lyrics = lyrics
+
+    def song_check(artist_id):
+        for i in songs():
+            if(self.artist_id == artist.id):
+                print(f'{self.title} is by {self.artist_id}')
+
 
