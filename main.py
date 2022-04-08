7f399c203857e795c5719ded24f2d138e7cff937
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-04-08 14:11:53 -0700

    Check is artist is present on list.

--- main.py
+++ main.py
@@ -5,42 +5,46 @@ class Artist:
 
     def __init__(self, name, genre, dob):
         self.name = name
+        Artist.artists.append(name)
         self.genre = genre 
         self.dob = dob
         self.artist_id = Artist.artist_sequence
         Artist.artist_sequence += 1
     
-    def artist_check():
-        
+    def artist_check(self):
+        if self.name in Artist.artists:
+            print(f'{self.name} is already on the list!')
+        else:
+            print('Artist not found.')
 
-
-
+taylor = Artist('Taylor Swift', 'Pop', '12/13/89')
+phoebe = Artist('Phoebe Bridgers', 'Alternative', '8/17/94')
 grimes = Artist('Grimes', 'Pop', '3/17/88')
 mitski = Artist('Mitski', 'Alternative', '9/27/90')
-
-print(names)
-
-# artists = []
-# artists.append(Artist('Taylor Swift', 'Pop', '12/13/89'))
-# artists.append(Artist('Phoebe Bridgers', 'Alternative', '8/17/94'))
-# artists.append(Artist('Mitski', 'Alternative', '9/27/90'))
-
-# grimes = Artist('Grimes', 'Pop', '3/17/88')
-# mitski = Artist('Mitski', 'Alternative', '9/27/90')
-
-# grimes.artist_check()
-# mitski.artist_check()
-
-# class Song:
-#     def __init__(self, artist_id, title, length, lyrics):
-#         self.artist_id = artist_id
-#         self.title = title
-#         self.length = length 
-#         self.lyrics = lyrics
-
-#     def song_check(artist_id):
-#         for i in songs():
-#             if(self.artist_id == artist.id):
-#                 print(f'{self.title} is by {self.artist_id}')
+grimes = Artist('Grimes', 'Pop', '3/17/88')
 
 
+
+
+class Song:
+    def __init__(self, artist_id, title, length, lyrics):
+        self.artist_id = artist_id
+        self.title = title
+        self.length = length 
+        self.lyrics = lyrics
+    def get_artist_id(self):
+        return self.artist_id
+    def get_title(self):
+        return self.title
+    def get_length(self):
+        return self.length
+    def get_lyrics(self):
+        return self.lyrics
+
+    def song_check(self):
+        if(self.artist_id == artist_id):
+            print(f'This song was created by {self.artist_id}')
+
+atw = Song('100', 'All To Well', '10:13', 'atwlyrics')
+
+atw.song_check()
